e5d343411e87183312974047cf3d99638c66e840
VG <vengadoravg@gmail.com>
2018-01-27 14:20:23 -0400

    prueba de git

---
+++ Assets/Test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Test : MonoBehaviour {
+    void Update () {
+        for (int i=0; i<transform.childCount; i++) {
+            transform.GetChild(i).transform.position = transform.position + transform.forward * i;
+        }
+        // alsdkfjleifjslidi
+    }
+}
